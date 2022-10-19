09e06a8ac94fd026c50c2aa65a0047c873d93854
Max Cohen <maxcohen@bu.edu>
2022-10-19 16:15:58 -0400

    Sims with aCBFs

--- src/ReinforcementLearning/critic_gradient_sims.jl
+++ src/ReinforcementLearning/critic_gradient_sims.jl
@@ -79,23 +79,35 @@ function (S::Simulation)(
     return trajectory
 end
 
-# function (S::Simulation)(
-#     Σ::ControlAffineSystem,
-#     k::CBFToolbox.CBFQuadProg, 
-#     ϕ::BasisFunctions,
-#     τC::CriticGradient,
-#     τA::ActorGradient,
-#     x::Union{Float64, Vector{Float64}},
-#     Wc::Vector{Float64},
-#     Wa::Vector{Float64},
-#     )
-#     # Construct right-hand-side
-#     rhs!(dX, X, p, t) = rhs_mbrl_gradient!(dX, X, p, t, Σ, k, ϕ, τC, τA)
-#     problem = ODEProblem(rhs!, vcat(x, Wc, Wa), S.tf)
-#     trajectory = solve(problem)
+function (S::Simulation)(
+    Σ::ControlAffineSystem,
+    P::MatchedParameters,
+    k::RACBFQuadProg, 
+    ϕ::BasisFunctions,
+    τC::CriticGradient,
+    τA::ActorGradient,
+    τ::ICLGradientUpdateLaw,
+    x::Union{Float64, Vector{Float64}},
+    Wc::Vector{Float64},
+    Wa::Vector{Float64},
+    θ̂::Union{Float64, Vector{Float64}}
+    )
+    # Construct right-hand-side
+    rhs!(dX, X, p, t) = rhs_mbrl_gradient!(dX, X, p, t, Σ, P, k, ϕ, τC, τA, τ)
 
-#     return trajectory
-# end
+    # Set up parameter dictionary
+    p = Dict("Σ" => Σ, "P" => P, "τ" => τ, "k" => k, "ϕ" => ϕ)
+
+    # Set up callback for updating history stack
+    ts = (S.t0 + τ.Δt) : τ.dt : S.tf
+    affect! = icl_cbf_mbrl_gradient_affect!
+    cb = PresetTimeCallback(ts, affect!)
+
+    problem = ODEProblem(rhs!, vcat(x, Wc, Wa, θ̂, P.ϑ), S.tf, p)
+    trajectory = solve(problem, callback=cb, tstops=ts)
+
+    return trajectory
+end
 
 function rhs_mbrl_gradient!(
     dX, 
@@ -211,36 +223,49 @@ function rhs_mbrl_gradient!(
     return nothing
 end
 
-# function rhs_mbrl_gradient!(
-#     dX, 
-#     X, 
-#     p, 
-#     t, 
-#     Σ::ControlAffineSystem, 
-#     k::CBFToolbox.CBFQuadProg, 
-#     ϕ::BasisFunctions,
-#     τC::CriticGradient,
-#     τA::ActorGradient,
-#     )
-#     # Pull out states
-#     x = Σ.n == 1 ? X[1] : X[1:Σ.n]
-#     Wc = X[Σ.n + 1 : Σ.n + ϕ.L]
-#     Wa = X[Σ.n + ϕ.L + 1 : Σ.n + 2*ϕ.L]
+function rhs_mbrl_gradient!(
+    dX, 
+    X, 
+    p, 
+    t, 
+    Σ::ControlAffineSystem,
+    P::MatchedParameters, 
+    k::RACBFQuadProg, 
+    ϕ::BasisFunctions,
+    τC::CriticGradient,
+    τA::ActorGradient,
+    τ::ICLGradientUpdateLaw,
+    )
+    # Pull out states
+    x = Σ.n == 1 ? X[1] : X[1:Σ.n]
+    Wc = X[Σ.n + 1 : Σ.n + ϕ.L]
+    Wa = X[Σ.n + ϕ.L + 1 : Σ.n + 2*ϕ.L]
+    θ̂ = P.p == 1 ? X[Σ.n + 2*ϕ.L + 1] : X[Σ.n + 2*ϕ.L + 1 : Σ.n + 2*ϕ.L + P.p]
+    ϑ = X[end]
 
-#     # Dynamics
-#     if Σ.n == 1
-#         dX[1] = Σ.f(x) + Σ.g(x)*k(x, Wa)
-#     else
-#         dX[1:Σ.n] = Σ.f(x) + Σ.g(x)*k(x, Wa)
-#     end
+    # Dynamics
+    if Σ.n == 1
+        dX[1] = Σ.f(x) + Σ.g(x)*(k(x, Wa, θ̂, ϑ) + P.φ(x)*P.θ)
+    else
+        dX[1:Σ.n] = Σ.f(x) + Σ.g(x)*(k(x, Wa, θ̂, ϑ) + P.φ(x)*P.θ)
+    end
 
-#     # Update laws
-#     dX[Σ.n + 1 : Σ.n + ϕ.L] = τC(x, Wc, Wa)
-#     dX[Σ.n + ϕ.L + 1 : Σ.n + 2*ϕ.L] = τA(Wa, Wc)
+    # Update laws
+    dX[Σ.n + 1 : Σ.n + ϕ.L] = τC(x, θ̂, Wc, Wa)
+    dX[Σ.n + ϕ.L + 1 : Σ.n + 2*ϕ.L] = τA(Wa, Wc)
 
-#     return nothing
-# end
+    # Update law
+    if P.p == 1
+        dX[Σ.n + 2*ϕ.L + 1] = τ(θ̂)
+    else
+        dX[Σ.n + 2*ϕ.L + 1 : Σ.n + 2*ϕ.L + P.p] = τ(θ̂)
+    end
 
+    # Estimation error
+    dX[end] = estimation_error_dyn(ϑ, τ)
+
+    return nothing
+end
 
 # Integrator callbacks
 """
@@ -265,6 +290,41 @@ function icl_mbrl_gradient_affect!(int)
             integrate_drift(x, int.t, int.t - int.p["τ"].Δt, int.p["Σ"]),
             F, 
             integrate_control(x, Wa, int.t, int.t - int.p["τ"].Δt, int.p["Σ"], int.p["k"])
+            )
+    end
+
+    return int
+end
+
+"""
+    icl_cbf_mbrl_gradient_affect!(integrator)
+
+Stop integration and update history stack with current data.
+"""
+function icl_cbf_mbrl_gradient_affect!(int)
+    # Pull out system dimensions
+    n = int.p["Σ"].n
+    L = int.p["ϕ"].L
+    p = int.p["P"].p
+
+    # Construct trajectory function
+    x(t) = n == 1 ? int.sol(t, idxs=1) : int.sol(t, idxs=1:n)
+
+    # Integrate regressor
+    F = integrate_regressor(x, int.t, int.t - int.p["τ"].Δt, int.p["Σ"], int.p["P"])
+
+    # Check if regressor is different enough to add data
+    if stack_difference(int.p["τ"].stack, F) >= int.p["τ"].stack.ε
+        # If so integrate other objects of interest and update stack
+        Wa(t) = int.sol(t, idxs=(n + L + 1 : n + 2*L))
+        θ̂(t) = p == 1 ? int.sol(t, idxs=(n + 2*L + 1)) : int.sol(t, idxs=(n + 2*L + 1 : n + 2*L + p))
+        ϑ(t) = int.sol(t, idxs=(n + 2*L + p + 1))
+        update_stack!(
+            int.p["τ"].stack, 
+            integrate_state_derivative(x,  int.t, int.t - int.p["τ"].Δt), 
+            integrate_drift(x, int.t, int.t - int.p["τ"].Δt, int.p["Σ"]),
+            F, 
+            integrate_control(x, Wa, θ̂, ϑ, int.t, int.t - int.p["τ"].Δt, int.p["Σ"], int.p["k"])
             )
     end
 
